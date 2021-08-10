5c92471e781b3e18566d12500643254ae7a20593
neverwinter-sjh <neverwinter.sjh@gmail.com>
2021-08-10 22:26:59 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const gulp = require("gulp");
+const clean = require("gulp-clean");
+const sass = require("gulp-dart-sass");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const browsersync = require("browser-sync").create();
+// const htmlPartial = require("gulp-html-partial");
+// const fileinclude = require("gulp-file-include");
+const include = require("gulp-html-tag-include");
+
+const src = {
+  sassPath: "sass/**/*.scss",
+  htmlPath: "src/**/*.html",
+  distPath: "css",
+  htmlDistPath: "html",
+  mapPath: ".",
+};
+
+gulp.task("html", function () {
+  return gulp
+    .src([src.htmlPath])
+    .pipe(include())
+    .pipe(gulp.dest(src.htmlDistPath));
+});
+
+// Compile SASS
+gulp.task("sass", () => {
+  return gulp
+    .src(src.sassPath)
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(sourcemaps.write(src.mapPath))
+    .pipe(gulp.dest(src.distPath))
+    .pipe(browsersync.reload({ stream: true }));
+});
+
+// Start App on Browser
+gulp.task("browser-sync", () => {
+  browsersync.init({
+    server: "./",
+  });
+});
+
+// Detect Changes
+gulp.task("watch", () => {
+  gulp.watch(src.sassPath, gulp.series("sass"));
+  gulp.watch(src.htmlPath, gulp.series("html"));
+  gulp.watch("src/**/*.html").on("change", browsersync.reload);
+});
+
+// Run Gulp Magic
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("sass", "html", "browser-sync", "watch"))
+);
